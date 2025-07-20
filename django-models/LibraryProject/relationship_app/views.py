de890f9d0d3a91d06932fafbccd6b875078f46b1
Atem Kuot <atemliaikuot@gmail.com>
2025-07-20 20:04:27 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,13 @@
 
 from django.shortcuts import render, redirect
-from .models import Library, Book
+from django.views import View
+from .models import Library, Book, UserProfile
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm,  AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import login
-
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.template import loader
 
 def list_books(request):
     """Function-based view to list all books."""
@@ -18,8 +20,6 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-from django.shortcuts import render, redirect
-from django.views import View
 
 # Registration View
 class RegisterView(View):
@@ -42,3 +42,28 @@ class UserLoginView(LoginView):
 # Logout View (uses built-in LogoutView)
 class UserLogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
+
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
