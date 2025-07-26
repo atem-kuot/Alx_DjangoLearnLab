df311dfe995c5a01b0193dc3822b3a0481f28c84
Atem Kuot <atemliaikuot@gmail.com>
2025-07-26 21:11:34 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -43,3 +43,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view article"),
+            ("can_create", "Can create article"),
+            ("can_edit", "Can edit article"),
+            ("can_delete", "Can delete article"),
+        ]
