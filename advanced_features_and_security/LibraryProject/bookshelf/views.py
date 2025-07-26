539f9c0b3866987a316ea260e6be5b339ad1b3ca
Atem Kuot <atemliaikuot@gmail.com>
2025-07-26 21:10:15 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Article
+from .forms import ArticleForm
 
-# Create your views here.
+@permission_required('news_app.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'news_app/article_list.html', {'articles': articles})
+
+@permission_required('news_app.can_create', raise_exception=True)
+def article_create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('article_list')
+    else:
+        form = ArticleForm()
+    return render(request, 'news_app/article_form.html', {'form': form})
+
+@permission_required('news_app.can_edit', raise_exception=True)
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    form = ArticleForm(request.POST or None, instance=article)
+    if form.is_valid():
+        form.save()
+        return redirect('article_list')
+    return render(request, 'news_app/article_form.html', {'form': form})
+
+@permission_required('news_app.can_delete', raise_exception=True)
+def article_delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect('article_list')
