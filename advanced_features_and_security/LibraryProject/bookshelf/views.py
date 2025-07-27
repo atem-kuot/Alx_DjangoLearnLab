444b5af1a0266d2aa75d9f3d3ad5430bcd5fe0ad
Atem Kuot <atemliaikuot@gmail.com>
2025-07-27 15:44:10 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from .forms import BookForm  # Assume you have a form
-from .forms import BookSearchForm
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
@@ -39,7 +39,7 @@ def book_delete(request, pk):
 
 
 def search_books(request):
-    form = BookSearchForm(request.GET or None)
+    form = ExampleForm(request.GET or None)
     results = []
     
     if form.is_valid():
