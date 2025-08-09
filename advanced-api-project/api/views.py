cb4b9eb85af38d97f42d7b8a9f92c4de5f87391f
atem-kuot <atemliaikuot@gmail.com>
2025-08-09 17:04:36 +0200

    add advanced_api_project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,4 @@
+from django_filters import rest_framework
 from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated 
 from django_filters.rest_framework import DjangoFilterBackend
@@ -81,4 +82,4 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]  # Only logged-in users can delete
-    
+
